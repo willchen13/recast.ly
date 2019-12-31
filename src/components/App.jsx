ea6d096a4323be01282f4fd6eaf938370adfabd5
willchen13 <williamchen13225@gmail.com>
2019-12-31 13:59:10 -0800

    Render VideoList, VideoListEntry, VideoPlayer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
-var App = () => (
+import VideoList from './VideoList.js';
+import videos from '../data/exampleVideoData.js';
+import VideoPlayer from './VideoPlayer.js';
+var App = (props) => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
@@ -7,10 +10,10 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em> <VideoPlayer video = {videos[0]}/></h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList</em> <VideoList videos = {videos}/></h5></div>
       </div>
     </div>
   </div>
